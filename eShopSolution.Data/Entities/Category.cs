f196a724c1db4cc0a1030a4b99f9b89490ea9061
long1712578 <pdlong578@gmail.com>
2020-11-10 13:50:12 +0700

    Create Entities

---
+++ eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using eShopSolution.Data.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int SortOrder { get; set; }
+        public bool IsShowOnHome { get; set; }
+        public int? ParenId { get; set; }
+        public Status Status { get; set; }
+
+    }
+}
